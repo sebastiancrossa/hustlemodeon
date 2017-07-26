5e9f6cadf8252632784a1b3d6a988e25a6e5cfeb
Sebastian C <crossasebastian@gmail.com>
2017-07-25 22:13:06 -0600

    Added graphics and simple animation

--- HustleMode/ViewController.swift
+++ HustleMode/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    
+    @IBOutlet weak var cloudHolder: UIView!
+    
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    var player : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func powerButtonPressed(_ sender: Any) {
+        
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerButton.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 100, width: 361, height: 457)
+        }) { (finished) in
+            self.hustleLbl.isHidden = false
+            self.onLbl.isHidden = false
+        }
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
 }
 
